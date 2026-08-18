eb4f87aac414172ffd9d6790e6f06d99dc313f9d
nanaezzze-star <nanaezzze@gmail.com>
2026-08-18 11:45:16 +0300

    add icon illustration components

---
+++ src/components/UI/icons/caseIcon.jsx
@@ -0,0 +1,18 @@
+export default function CaseIcon(){
+    return(
+    <svg
+      width="56"
+      height="48"
+      viewBox="0 0 56 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path
+        d="M36 18H33V16C33 14.897 32.103 14 31 14H25C23.897 14 23 14.897 23 16V18H20C18.897 18 18 18.897 18 20V23H38V20C38 18.897 37.103 18 36 18ZM25 16H31V18H25V16ZM30 26H26V24H18V31C18 32.103 18.897 33 20 33H36C37.103 33 38 32.103 38 31V24H30V26Z"
+        fill="currentColor"
+      />
+    </svg>
+
+    )
+}
---
+++ src/components/UI/icons/searchIcon.jsx
@@ -0,0 +1,26 @@
+export default function SearchIcon (){
+    return(
+        <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+    >
+      <path
+        d="M16.9492 16.9492L20.9992 20.9992"
+        stroke="#9692A8"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M19.2 11.1C19.2 6.62649 15.5735 3 11.1 3C6.62649 3 3 6.62649 3 11.1C3 15.5735 6.62649 19.2 11.1 19.2C15.5735 19.2 19.2 15.5735 19.2 11.1Z"
+        stroke="#9692A8"
+        strokeWidth="1.5"
+        strokeLinejoin="round"
+      />
+    </svg>
+    )
+}
