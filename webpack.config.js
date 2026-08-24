840b8557ae65c29671979a11a23ae43a9cd8bf0e
nanaezzze-star <nanaezzze@gmail.com>
2026-08-24 17:26:38 +0300

    change wepbpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,64 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV === 'production';
+module.exports = (_env, argv) => {
+  const isProduction = argv.mode === 'production';
 
-module.exports = {
-  mode: isProduction ? 'production' : 'development',
+  return {
+    mode: isProduction ? 'production' : 'development',
 
-  entry: './src/Main.jsx',
-  
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/', 
-    clean: true,
-  },
-  
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              '@babel/preset-env',
-              [
-                '@babel/preset-react', 
-                { 
-                  runtime: 'automatic',
-                  development: !isProduction 
-                }
-              ]
-            ]
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ],
-  
-  devServer: {
-    port: 3000,
-    historyApiFallback: true,
-    hot: true,
-    open: true
-  }
+    entry: './src/Main.jsx',
+
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: '/',
+      clean: true,
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env',
+                [
+                  '@babel/preset-react',
+                  {
+                    runtime: 'automatic',
+                    development: !isProduction,
+                  },
+                ],
+              ],
+            },
+          },
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
+
+    devServer: {
+      port: 3000,
+      historyApiFallback: true,
+      hot: true,
+      open: true,
+    },
+  };
 };
